8c7538c6ce0f39379652e43c76fb1561a09585db
Update56 <heupdate56@gmail.com>
2023-07-08 17:30:56 +0700

    Добавьте файлы проекта.

---
+++ Code/Master.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project4.Code
+{
+    enum Skill
+    {
+        novice,
+        skilled,
+        expert,
+        pro
+    }
+    enum Qual
+    {
+        haircutter,
+        visagiste,
+        manicurist
+    }
+    internal class Master
+    {
+
+        Skill skill;
+        Qual qual;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        using var game = new Project4.Game1();
+        game.Run();
+    }
+}
